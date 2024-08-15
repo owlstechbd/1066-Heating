5ae78e4d65c0f5a8a8788f0e1cc4b204f059c78c
owlstechbd <owlstech.net@gmail.com>
2024-08-15 12:31:38 +0600

    responsive all done now

--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
 var elem = document.querySelector('.testimonial-container');
-        var msnry = new Masonry(elem, {
-            itemSelector: '.testimonial-item',
-            columnWidth: '.testimonial-item',
-            percentPosition: true,
-            gutter: 10 // Adjust as needed
-        });
+var msnry = new Masonry(elem, {
+    itemSelector: '.testimonial-item',
+    columnWidth: '.testimonial-item',
+    percentPosition: true,
+    gutter: 10 // Adjust as needed
+});
 
-        // Initialize Masonry after all images are loaded
-        imagesLoaded(elem, function() {
-            msnry.layout();
-        });
+// Initialize Masonry after all images are loaded
+imagesLoaded(elem, function() {
+    msnry.layout();
+});
+
+// Re-layout Masonry when the window is resized
+window.addEventListener('resize', function() {
+    msnry.layout();
+});
 
   new WOW().init();
